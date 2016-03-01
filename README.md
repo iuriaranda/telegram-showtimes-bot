535dce26660992ac59992acceabe3261fb3531ee
iuri aranda <iuri.aranda@gmail.com>
2016-03-01 16:10:45 +0100

    Lots of changes

--- README.md
+++ README.md
@@ -3,16 +3,38 @@ telegram-showtimes-bot
 
 ## Commands
 
-### /setlocation
+### /setlocation [location]
+
+Sets the location specified in the text. `location` can be a city and country names, a postal code, or latitude and longitude (separated by ,).
+
+##### Responses:
+ - Your location has been successfully set to __your location__
+ - There has been an error setting your location, please try again later
+
+### location message sent
+
+Sets the user's location using the coordinates specified in the location message.
+
+##### Responses:
+ - Your location has been successfully set to __your location__
+ - There has been an error setting your location, please try again later
 
 ### /movies or /showtimes [query]
 
-Shows showtimes in near theaters
+Shows movie showtimes and nearby theaters screening them. You can filter the movies and dates with `query`, for example `/movies tomorrow` will show showtimes for tomorrow, and `/movies star wars` will only show showtimes for movies containing star wars in their title.
+
+##### Responses:
+- Here are the showtimes for __today__ in __location__:
+- There has been an error retrieving showtimes, please try again later
 
 ### /theaters [query]
 
-Shows nearby theaters
+Shows nearby theaters and the movies they are screening. You can filter the theaters and dates with `query`, for example `/theaters tomorrow` will show theaters and their showtimes for tomorrow, and `/theaters Verdi` will only show nearby theaters called Verdi.
 
-# todo send a second command to show showtimes for other day
+##### Responses:
+- Here are the theaters screening __today__ in __location__:
+- There has been an error retrieving showtimes, please try again later
 
-# License
+## License
+
+MIT
--- index.js
+++ index.js
@@ -23,13 +23,21 @@ lambdaConfig.on('ready', function () {
   bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN || CONFIG.telegram_bot_key, { polling: false, webhook: false });
 });
 
-var help_text = 'I can help you find nearby theaters and showtimes. Tell me, what are you looking for?\n\n';
-help_text += '/setlocation - Sets your location for future reference\n';
-help_text += '/movies or /showtimes - Shows the movies that are projecting in near theaters\n';
-help_text += '/theaters - Shows nearby theaters, and the movies they are screening\n';
+var help_text = 'I can show you nearby theaters and showtimes. Tell me, what can I do for you?\n\n';
+help_text += '-> /movies or /showtimes \\[query\] - Shows movie showtimes and nearby theaters screening them. You can filter the movies and dates with `query`, for example `/movies tomorrow` will show showtimes for tomorrow, and `/movies star wars` will only show showtimes for movies containing star wars in their title.\n\n';
+help_text += '-> /theaters \\[query\] - Shows nearby theaters and the movies they are screening. You can filter the theaters and dates with `query`, for example `/theaters tomorrow` will show theaters and their showtimes for tomorrow, and `/theaters Verdi` will only show nearby theaters called Verdi.\n\n';
+help_text += '-> /setlocation - Sets your location for future reference';
 
-var empty_movies_text = 'Sorry, I couldn\'t find any showtimes matching %s';
-var empty_theaters_text = 'Sorry, I couldn\'t find any theaters matching %s';
+var deferred_location_help_text = 'Send me your location using the location button on Telegram, or send it manually with "/setlocation city, country or zip code".';
+
+var empty_text = 'Sorry, I couldn\'t find any %s matching %s.';
+var no_location_text = 'These are not the theaters you\'re looking for...\nTo set your preferred location, use the /setlocation command.';
+var error_showtimes_text = 'Error finding showtimes. Please try again in a few minutes.';
+var error_theaters_text = 'Error finding theaters. Please try again in a few minutes.';
+var error_setting_location_text = 'An error happened setting your new location, please try again in a few minutes.';
+var location_set_text = 'I\'ve set your location to %s.';
+
+var default_location = 'Barcelona, Spain';
 
 // Process unhandled messages
 var onMessage = function (msg, cb) {
@@ -44,84 +52,73 @@ var onMessage = function (msg, cb) {
 };
 
 var onSetLocation = function (msg, cb, matches) {
-  setLocation(msg.chat.id, matches[1], function (err) {
-    var response;
-    if (err) {
-      console.log(err);
-      response = bot.sendMessage(msg.chat.id, 'An error happened setting your new location, please try again in a few moments');
-    } else {
-      response = bot.sendMessage(msg.chat.id, util.format('I\'ve set your location to %s', matches[1]));
-    }
-
-    response.then(function () {
-      cb();
-    }).catch(cb);
-  });
+  setLocation(msg.chat.id, matches[1], cb);
 };
 
 var onSetDeferedLocation = function (msg, cb) {
-  bot.sendMessage(msg.chat.id, 'Send me your location using the location button on Telegram, or send it manually with "/setlocation city, country or zip code"').then(function () {
+  bot.sendMessage(msg.chat.id, deferred_location_help_text).then(function () {
     cb();
   }).catch(cb);
 };
 
 var onLocation = function (msg, cb) {
   if (msg.chat.type !== 'private') return cb(); // Ignore all but private messages, TODO: be able to set location in a group
-
-  setLocation(msg.chat.id, util.format('%s,%s', msg.location.latitude, msg.location.longitude), function (err) {
-    var response;
-    if (err) {
-      console.log(err);
-      response = bot.sendMessage(msg.chat.id, 'An error happened setting your new location, please try again in a few moments');
-    } else {
-      response = bot.sendMessage(msg.chat.id, 'I\'ve set your location');
-    }
-
-    response.then(function () {
-      cb();
-    }).catch(cb);
-  });
+  setLocation(msg.chat.id, util.format('%s,%s', msg.location.latitude, msg.location.longitude), cb);
 };
 
 var onTheaters = function (msg, cb, matches) {
   bot.sendChatAction(msg.chat.id, 'typing');
 
-  api.getTheatersAsync(matches[1]).then(function (theaters) {
-    if (theaters.length) {
-      return Promise.mapSeries(formatTheaters(msg, theaters), function (text) {
-        return bot.sendMessage(msg.chat.id, text, {
-          parse_mode: 'Markdown',
-          disable_web_page_preview: true
-        });
-      });
-    } else {
-      return bot.sendMessage(msg.chat.id, util.format(empty_theaters_text, matches[1]));
+  var no_location = false;
+  getLocationForUser(msg.chat.id).then(function (location) {
+    if (!location) {
+      location = default_location;
+      no_location = true;
     }
+
+    var api = new Showtimes(location);
+    return api.getTheatersAsync(matches[1]);
+  }).then(function (theaters) {
+    var response = formatTheaters(msg, theaters, matches[1]);
+    if (no_location) response.push(no_location_text);
+    return Promise.mapSeries(response, function (text) {
+      return bot.sendMessage(msg.chat.id, text, {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true
+      });
+    });
   }).then(function () {
     cb();
   }).catch(function (err) {
-    onError(err, msg, 'Error fetching theaters', cb);
+    onError(err, msg, error_theaters_text, matches[1], 'theaters', cb);
   });
 };
 
 var onShowtimes = function (msg, cb, matches) {
   bot.sendChatAction(msg.chat.id, 'typing');
 
-  api.getMoviesAsync(matches[1]).then(function (movies) {
-    if (movies.length) {
-      return Promise.mapSeries(formatMovies(msg, movies), function (text) {
-        return bot.sendMessage(msg.chat.id, text, {
-          parse_mode: 'Markdown',
-          disable_web_page_preview: true
-        });
-      });
-    } else {
-      return bot.sendMessage(msg.chat.id, util.format(empty_movies_text, matches[1]));
+  var no_location = false;
+  getLocationForUser(msg.chat.id).then(function (location) {
+    if (!location) {
+      location = default_location;
+      no_location = true;
     }
+
+    var api = new Showtimes(location);
+    return api.getMoviesAsync(matches[1]);
+  }).then(function (movies) {
+    var response = formatMovies(msg, movies, matches[1]);
+    if (no_location) response.push(no_location_text);
+    return Promise.mapSeries(response, function (text) {
+      return bot.sendMessage(msg.chat.id, text, {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true
+      });
+    });
   }).then(function () {
     cb();
   }).catch(function (err) {
-    onError(err, msg, 'Error fetching theaters', cb);
+    onError(err, msg, error_showtimes_text, matches[1], 'showtimes', cb);
   });
 };
 
@@ -142,26 +139,56 @@ var setLocation = function (id, location, cb) {
     ExpressionAttributeNames: {
       '#loc': 'location'
     }
-  }, cb);
+  }, function (err) {
+    if (err) console.log(err);
+    bot.sendMessage(id, err ? error_setting_location_text : util.format(location_set_text, location)).then(function () {
+      cb();
+    }).catch(cb);
+  });
 };
 
-var onError = function (err, msg, err_msg, cb) {
-  console.error(err);
+var getLocationForUser = function (user_id) {
+  return new Promise(function (fulfill, reject) {
+    db.getItem({
+      Key: {
+        userid: {
+          N: user_id + ''
+        }
+      },
+      TableName: 'showtimesbot-users'
+    }, function (err, data) {
+      if (err) reject(err);
+      else if (data.Item && data.Item.location && data.Item.location.S) fulfill(data.Item.location.S);
+      else fulfill(null);
+    });
+  });
+};
+
+var onError = function (err, msg, err_msg, query, type, cb) {
+  if (/Your query .+ did not match|No .+ were found/i.test(err)) {
+    err_msg = util.format(empty_text, type, query);
+  } else {
+    console.error(err);
+  }
+
   bot.sendMessage(msg.chat.id, err_msg).then(function () {
     cb();
   }).catch(cb);
 };
 
-var formatThings = function (msg, things, type) {
-  var response = [util.format('Here are the %s projecting near you:\n', type)];
+var formatThings = function (msg, things, type, query) {
+  var response = [ util.format('Here are the showtimes for %s near %s:\n', things.date, things.location) ];
   var otherType = type === 'movies' ? 'theaters' : 'movies';
   var mod = type === 'movies' ? 10 : 5;
+  var noThings = true;
 
-  things.forEach(function (thing) {
-    response.push(util.format('*%s*\n', thing.name));
-    var i = response.length - 1;
+  things.data.forEach(function (thing) {
+    var partialResponse = util.format('*%s*\n', thing.name);
+    var noOtherThings = true;
     if (thing[otherType]) {
       thing[otherType].forEach(function (otherThing) {
+        if (otherThing.name === '' || (otherThing.showtimes.length === 1 && otherThing.showtimes[0] === '')) return;
+        noOtherThings = false;
         var showtimes = [];
         if (otherThing.showtime_tickets) {
           otherThing.showtimes.forEach(function (time) {
@@ -170,25 +197,35 @@ var formatThings = function (msg, things, type) {
         } else {
           showtimes = otherThing.showtimes;
         }
-        response[i] += util.format('%s - %s\n', otherThing.name, showtimes.join(' '));
+        partialResponse += util.format('%s - %s\n', otherThing.name, showtimes.join(' '));
       });
     }
-  });
-  return _.compact(_.map(response, function (line, l) {
-    if (l%mod === mod - 1) {
-      return response.slice(l - l%mod, l + 1).join('\n');
-    } else if (l === response.length - 1) {
-      return response.slice(l - l%mod).join('\n');
+
+    if (!noOtherThings) {
+      response.push(partialResponse);
+      noThings = false;
     }
-  }));
+  });
+
+  if (noThings) {
+    return [ util.format(empty_text, type, query) ];
+  } else {
+    return _.compact(_.map(response, function (line, l) {
+      if (l%mod === mod - 1) {
+        return response.slice(l - l%mod, l + 1).join('\n');
+      } else if (l === response.length - 1) {
+        return response.slice(l - l%mod).join('\n');
+      }
+    }));
+  }
 };
 
-var formatMovies = function (msg, movies) {
-  return formatThings(msg, movies, 'movies');
+var formatMovies = function (msg, movies, query) {
+  return formatThings(msg, movies, 'movies', query);
 };
 
-var formatTheaters = function (msg, theaters) {
-  return formatThings(msg, theaters, 'theaters');
+var formatTheaters = function (msg, theaters, query) {
+  return formatThings(msg, theaters, 'theaters', query);
 };
 
 exports.handler = lambdaConfig.handler(telegramHandler({
